71777f823b29a0298225d50204905c2e316e684d
harryk2142 <harry_k_89@outlook.de>
2023-05-14 11:47:31 +0200

    more description + styling

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,8 @@
-export const SITE_TITLE = "MY BLOG";
-export const SITE_DESCRIPTION = "Welcome to my blog!";
+export const SITE_TITLE = "the truth is out there";
+export const SITE_DESCRIPTION =
+  "Willkommen auf dem Blog von Harry Kramer, dem Ort für außergewöhnlichen und satirischen Content, der mithilfe von KI erstellt wird! Tauche ein in eine Welt voller Kuriositäten, skurriler Geschichten und humorvoller Abenteuer, die von künstlicher Intelligenz zum Leben erweckt werden. " +
+  "Dieser Blog ist der Treffpunkt für all diejenigen, die nach einem Schmunzeln, einem herzhaften Lachen und einer Prise absurder Unterhaltung suchen. Hier erfährst du von den neuesten Trends in der AI-Welt, während wir die Grenzen der Kreativität mit unserer eigenen AI-Textgenerierung sprengen. " +
+  "Erwarte witzige Geschichten, die von Robotern mit einem Hauch von Ironie und Satire erzählt werden. Wir erkunden die unkonventionellsten Themen, von UFO-Sichtungen in der Nachbarschaft bis hin zu den seltsamsten Haustiergewohnheiten. Unsere AI-basierten Beiträge versetzen dich in eine Welt, in der das Ungewöhnliche zur Normalität wird und das Lachen nie aufhört. " +
+  "Entdecke unsere Kolumnen, in denen wir die absurdesten Nachrichtenüberschriften präsentieren oder surreal anmutende Alltagsszenarien humorvoll beleuchten. Wir laden dich ein, Teil einer Community zu sein, die den Wert des Lachens und der außergewöhnlichen Geschichten schätzt. " +
+  "Begib dich auf eine Reise in eine Welt voller künstlicher Intelligenz und sarkastischem Charme. Lass dich von unseren AI-generierten Artikeln überraschen, inspirieren und zum Lachen bringen. Bereite dich darauf vor, das Gewöhnliche zu verlassen und das Ungewöhnliche zu umarmen. " +
+  "Bist du bereit für ein humorvolles Abenteuer? Dann folge uns auf [Blogname], wo AI und Satire auf unerwartete Weise aufeinandertreffen!";

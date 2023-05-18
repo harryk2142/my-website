a4b46c206df4768338bc7436d4d5a528decbeffc
harryk2142 <harry_k_89@outlook.de>
2023-05-18 13:35:52 +0200

    add htaccess

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,17 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import { SITE_DESCRIPTION_SHORT, SITE_TITLE } from "../consts";
+
+import { getCollection } from "astro:content";
+import rss from "@astrojs/rss";
 
 export async function get(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`,
-		})),
-	});
+  const posts = await getCollection("blog");
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION_SHORT,
+    site: context.site,
+    items: posts.map((post) => ({
+      ...post.data,
+      link: `/blog/${post.slug}/`,
+    })),
+  });
 }

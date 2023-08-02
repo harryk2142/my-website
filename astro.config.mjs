b9fea9248708de62dad1c33a368539e2d82ba9aa
harryk2142 <harry_k_89@outlook.de>
2023-08-02 10:17:03 +0200

    try to deploy github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,34 +3,33 @@ import dotenv from "dotenv";
 import mdx from "@astrojs/mdx";
 import rehypeAstroRelativeMarkdownLinks from "astro-rehype-relative-markdown-links";
 import sitemap from "@astrojs/sitemap";
-
+import robotsTxt from "astro-robots-txt";
 dotenv.config(); // load env vars from .env
+
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://harry-kramer.net",
-  integrations: [
-    mdx({
-      remarkPlugins: [],
-      rehypePlugins: [rehypeAstroRelativeMarkdownLinks],
-    }),
-    sitemap({
-      filter: (page) => !page.includes("https://harry-kramer.net/tags"),
-    }),
-  ],
+  site: "https://harryk2142.github.io",
+  integrations: [mdx({
+    remarkPlugins: [],
+    rehypePlugins: [rehypeAstroRelativeMarkdownLinks]
+  }), sitemap({
+    filter: page => !page.includes("tags") && !page.includes("api")
+  }), robotsTxt()],
   vite: {
     define: {
       FB_API_KEY: JSON.stringify(process.env.FIREBASE_COMMENTS_API_KEY),
-      FB_PROJECT_ID: JSON.stringify(process.env.FIREBASE_COMMENTS_PROJECT_ID),
+      FB_PROJECT_ID: JSON.stringify(process.env.FIREBASE_COMMENTS_PROJECT_ID)
     },
     plugins: [],
     build: {
-      modulePreload: false,
+      modulePreload: false
       // reportCompressedSize: true,
-    },
+    }
   },
+
   markdown: {
     remarkPlugins: [],
-    rehypePlugins: [rehypeAstroRelativeMarkdownLinks],
-  },
+    rehypePlugins: [rehypeAstroRelativeMarkdownLinks]
+  }
 });
